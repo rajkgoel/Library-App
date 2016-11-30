3595324b2af7848536838356d655281699111000
rajkgoel <rajkgoel>
2016-11-30 06:54:34 -0600

    formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { Book } from './classes/book';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Sample App</h1>
-              <div *ngFor="let book of Books">
-                <div><h2>{{book.Name}} - {{book.Author | uppercase }}</h2></div>
-              </div>
-              `,
+  template: 
+  `<h1>Sample App</h1>
+    <div *ngFor="let book of Books">
+      <div><h2>{{book.Name}} - {{book.Author | uppercase }}</h2></div>
+    </div>
+  `,
   styles: [`h1 { color: red } 
             h2 { font-size: 20px; color: blue }`],
   providers: [ BookService ]
